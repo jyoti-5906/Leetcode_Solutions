66585925975e828e3fa603a7a76dcb039192c2d6
Jyoti <jyotikarayat05@gmail.com>
2026-09-21 11:43:40 +0530

    Time: 14 ms (92.79%) | Memory: 51.5 MB (56.27%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -6,16 +6,18 @@ class Solution {
 
         int child = 0;
         int cookie = 0;
+        int count = 0;
 
         while (child < g.length && cookie < s.length) {
             
             if (s[cookie] >= g[child]) {
+                count++;
                 child++;
             }
             
             cookie++;
         }
 
-        return child;
+        return count;
     }
 }
